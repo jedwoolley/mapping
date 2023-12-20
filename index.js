24518a2dff4862fc3b5dbb656d4458deb2dfa5cd
Jed Woolley <262685+jedwoolley@users.noreply.github.com>
2023-12-20 15:47:01 -0700

    getting the hang of it

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [-74.5, 40],
+    zoom: 9
+  });
 
-let myNum = 0
-function increment() {
-    myNum = myNum + 1
-    document.getElementById("countEl").innerText = myNum
-}
-
-function reset() {
-    myNum = 0
-    document.getElementById("countEl").innerText = 0
-}
-
-
+  map.on('load', () => {
+    map.addLayer({
+      id: 'rpd_parks',
+      type: 'fill',
+      source: {
+        type: 'vector',
+        url: 'mapbox://mapbox.3o7ubwm8'
+      },
+      'source-layer': 'RPD_Parks',
+      layout: {
+        visibility: 'visible'
+      },
+      paint: {
+        'fill-color': 'rgba(61,153,80,0.55)'
+      }
+    });
+  });
