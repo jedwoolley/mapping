9afeb389e930ec87664b40b9ad8ab8e436c8b605
Jed Woolley <262685+jedwoolley@users.noreply.github.com>
2023-12-20 15:35:38 -0700

    struggling with basics

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let myNum = 0
+function increment() {
+    myNum = myNum + 1
+    document.getElementById("countEl").innerText = myNum
+}
+
+function reset() {
+    myNum = 0
+    document.getElementById("countEl").innerText = 0
+}
+
+
